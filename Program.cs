5a2bd99fb60baf2dc3a2c68a64e3fe7614a5c8c4
Max <max.ronald9@gmail.com>
2023-08-22 20:19:11 +0300

    LHT-1 Старт проекта
    Убраны лишние файлы.
    Добавлены необходимые для работы пакеты.
    Задана версия приложения.
    Добавлен проект уровня бизнес-логики.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,19 @@
-﻿using Life_Habit_Tracker;
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Жизнь возобновилась");
 
-        Client sasha = new Client(1, "Саша");
+        //Client sasha = new Client(1, "Саша");
 
-        sasha.MakeHabit("Спортивная тренировка");
+        //sasha.MakeHabit("Спортивная тренировка");
 
         Console.WriteLine("Хотите ли вы осуществить привычку?");
         string? answer = Console.ReadLine();
         if (answer != null) {
             string? upperAnswer = answer.ToUpper();
             if (upperAnswer == "ДА") {
-                sasha.Execute();
+                //sasha.Execute();
             }
             else
             {

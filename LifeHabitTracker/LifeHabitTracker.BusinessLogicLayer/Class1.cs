ca5a6fd985661235f3d1359e17b4cb2c4c95040d
Alex Savin <2savinbuslov2@gmail.com>
2023-08-23 12:24:18 +0300

    Пробный пул

--- LifeHabitTracker/LifeHabitTracker.BusinessLogicLayer/Class1.cs
+++ LifeHabitTracker/LifeHabitTracker.BusinessLogicLayer/Class1.cs
@@ -2,6 +2,7 @@
 {
     public class Class1
     {
-
+        public int ID { get; set; }
+        public string Name { get; set; }
     }
 }
--- LifeHabitTracker/LifeHabitTracker.Console/Program.cs
+++ LifeHabitTracker/LifeHabitTracker.Console/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Саня, привет! Стартуем!");
+Console.WriteLine("Да, Максон! Стартуем!");
